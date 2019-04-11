698d22571a8714cdb16739d9999db8b052a2f9d8
Vieru Andrei <mixerandr3w001@gmail.com>
2019-04-11 16:08:40 +0000

    am sters niste comentarii

--- src/boot/analytics.js
+++ src/boot/analytics.js
@@ -1,7 +1,5 @@
-// import something here
 import VueAnalytics from "vue-analytics";
 
-// leave the export, even if you don't use it
 export default async ({ app, router, Vue }) => {
   Vue.use(VueAnalytics, {
     id: "UA-133910194-1",
